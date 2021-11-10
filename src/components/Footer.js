18fdb8ec75a1bc56ffea283527951899ec25a3c4
Mischa Helfenstein <helfenstein.mischa@gmail.com>
2021-11-10 23:28:21 +0100

    Add some footer info.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import styled from 'styled-components';
 import ContentContainer from './ContentContainer';
 
+const FooterStyles = styled.div`
+  margin-bottom: 20px;
+`;
+
 export default function Footer() {
+  const year = new Date().getFullYear();
   return (
     <ContentContainer>
-      Heya!
+      <FooterStyles>
+        &copy;
+        {' '}
+        {year}
+        {' '}
+        - Mischa Helfenstein.
+      </FooterStyles>
     </ContentContainer>
   );
 }
