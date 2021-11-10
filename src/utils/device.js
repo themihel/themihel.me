cfd0a0895baa68d2890b8aa27194169c523cd79f
Mischa Helfenstein <helfenstein.mischa@gmail.com>
2021-11-10 22:41:25 +0100

    Add bootstrap breakpoints to adapt legacy code

--- src/utils/device.js
+++ src/utils/device.js
@@ -1,7 +1,11 @@
 const size = {
-  screenMdMin: '992px',
+  screenSmMin: '576px',
+  screenMdMin: '768px',
+  screenLgMin: '992px',
 };
 
 export const device = {
+  screenSmMin: `(min-width: ${size.screenSmMin})`,
   screenMdMin: `(min-width: ${size.screenMdMin})`,
+  screenLgMin: `(min-width: ${size.screenLgMin})`,
 };
