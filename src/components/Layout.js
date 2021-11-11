7b46bd6cf430ee766f92016276f72cb2002c8825
Mischa Helfenstein <helfenstein.mischa@gmail.com>
2021-11-12 00:00:22 +0100

    Improve font loading, remove unused font
    Article: https://css-tricks.com/how-to-load-fonts-in-a-way-that-fights-fout-and-makes-lighthouse-happy/

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -11,7 +11,26 @@ export default function Layout({ children }) {
       <Helmet>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,800;1,800&family=Roboto:wght@300;400&display=swap" rel="stylesheet" />
+
+        <link
+          rel="preload"
+          as="style"
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400&display=swap"
+        />
+
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400&display=swap"
+          media="print"
+          onLoad="this.media='all'"
+        />
+
+        <noscript>
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Merriweather&display=swap"
+          />
+        </noscript>
       </Helmet>
       <GlobalStyles />
       <Typography />
