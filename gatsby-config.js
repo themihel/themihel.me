fecdcd91e5c85842872c518322bbf4178a5b379d
Mischa Helfenstein <helfenstein.mischa@gmail.com>
2021-12-06 16:10:27 +0100

    Add maskable icons

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,24 +43,48 @@ module.exports = {
         background_color: '#000000',
         theme_color: '#000000',
         display: 'standalone',
-        icon: 'static/favicon.png',
-        icons: [{
-          src: 'static/launcher-icons/icon-128x128.png',
-          sizes: '128x128',
-          type: 'image/png',
-        }, {
-          src: 'static/launcher-icons/apple-touch-icon.png',
-          sizes: '152x152',
-          type: 'image/png',
-        }, {
-          src: 'static/launcher-icons/ms-touch-icon-144x144-precomposed.png',
-          sizes: '144x144',
-          type: 'image/png',
-        }, {
-          src: 'static/launcher-icons/chrome-touch-icon-192x192.png',
-          sizes: '192x192',
-          type: 'image/png',
-        }],
+        include_favicon: false,
+        icon: 'static/maskable/icon.png',
+        icon_options: {
+          purpose: 'any maskable',
+        },
+        icons: [
+          {
+            src: 'static/lmaskable/icon_x48.png',
+            sizes: '48x48',
+            type: 'image/png',
+          },
+          {
+            src: 'static/lmaskable/icon_x72.png',
+            sizes: '72x72',
+            type: 'image/png',
+          },
+          {
+            src: 'static/lmaskable/icon_x96.png',
+            sizes: '96x96',
+            type: 'image/png',
+          },
+          {
+            src: 'static/lmaskable/icon_x128.png',
+            sizes: '128x128',
+            type: 'image/png',
+          },
+          {
+            src: 'static/lmaskable/icon_x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'static/lmaskable/icon_x384.png',
+            sizes: '384x384',
+            type: 'image/png',
+          },
+          {
+            src: 'static/lmaskable/icon_x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
       },
     },
   ],
