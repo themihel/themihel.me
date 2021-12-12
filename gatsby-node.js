42d5d7352b349a252754231908df1d8bb23cf60f
Mischa Helfenstein <helfenstein.mischa@gmail.com>
2021-12-12 16:41:27 +0100

    Add basic implementation of static pages through CMS

---
+++ gatsby-node.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+exports.createPages = async ({ graphql, actions }) => {
+  console.info('[CreateStaticPages] Start creating static pages');
+
+  const { createPage } = actions;
+  const staticPageTemplate = path.resolve('./src/templates/StaticPage.js');
+  const { data } = await graphql(`
+    query {
+      staticPages: allSanityStaticPage {
+        nodes {
+          id
+          title
+          slug {
+            current
+          }
+        }
+      }
+    }
+  `);
+
+  data.staticPages.nodes.forEach((staticPage) => {
+    createPage({
+      path: `${staticPage.slug.current}`,
+      component: staticPageTemplate,
+      context: {
+        staticPageId: staticPage.id,
+      },
+    });
+
+    console.info(
+      `[CreateStaticPages] Created page for ${staticPage.title} (path: /${staticPage.slug.current})`,
+    );
+  });
+};
---
+++ src/templates/StaticPage.js
@@ -0,0 +1,31 @@
+import { graphql } from 'gatsby';
+import React from 'react';
+import BlockContent from '@sanity/block-content-to-react';
+import SEO from '../components/SEO';
+import ContentContainer from '../components/ContentContainer';
+
+export default function StaticPage({ data: { staticPage } }) {
+  return (
+    <>
+      <SEO title={staticPage.title} />
+
+      <ContentContainer>
+        <h1>{staticPage.title}</h1>
+        <BlockContent blocks={staticPage._rawContent} ignoreUnknownTypes />
+      </ContentContainer>
+    </>
+  );
+}
+
+export const query = graphql`
+  query ($staticPageId: String!) {
+    staticPage: sanityStaticPage(id: { eq: $staticPageId }) {
+      id
+      title
+      slug {
+        current
+      }
+      _rawContent
+    }
+  }
+`;
