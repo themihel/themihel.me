32df265efc5363fb366369de82bb017c23c8743e
Mischa Helfenstein <helfenstein.mischa@gmail.com>
2022-05-11 17:26:23 +0200

    Fix typo

--- gatsby-config.js
+++ gatsby-config.js
@@ -53,37 +53,37 @@ module.exports = {
         },
         icons: [
           {
-            src: 'static/lmaskable/icon_x48.png',
+            src: 'static/maskable/icon_x48.png',
             sizes: '48x48',
             type: 'image/png',
           },
           {
-            src: 'static/lmaskable/icon_x72.png',
+            src: 'static/maskable/icon_x72.png',
             sizes: '72x72',
             type: 'image/png',
           },
           {
-            src: 'static/lmaskable/icon_x96.png',
+            src: 'static/maskable/icon_x96.png',
             sizes: '96x96',
             type: 'image/png',
           },
           {
-            src: 'static/lmaskable/icon_x128.png',
+            src: 'static/maskable/icon_x128.png',
             sizes: '128x128',
             type: 'image/png',
           },
           {
-            src: 'static/lmaskable/icon_x192.png',
+            src: 'static/maskable/icon_x192.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: 'static/lmaskable/icon_x384.png',
+            src: 'static/maskable/icon_x384.png',
             sizes: '384x384',
             type: 'image/png',
           },
           {
-            src: 'static/lmaskable/icon_x512.png',
+            src: 'static/maskable/icon_x512.png',
             sizes: '512x512',
             type: 'image/png',
           },
